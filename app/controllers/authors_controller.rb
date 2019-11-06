94bd075958caf91792062f823331e0551f42a3fa
phoeinx <hradilak@uni-potsdam.de>
2019-11-06 14:05:20 +0100

    Close #7

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,6 +12,10 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
+  def edit
+    @author = Author.find(params[:id])
+  end
+
   def create
     @author = Author.new(author_params)
 
@@ -20,7 +24,16 @@ class AuthorsController < ApplicationController
     else
       render 'new'
     end
+  end
 
+  def update
+    @author = Author.find(params[:id])
+
+    if @author.update(author_params)
+      redirect_to @author
+    else
+      render 'edit'
+    end
   end
 
   private
