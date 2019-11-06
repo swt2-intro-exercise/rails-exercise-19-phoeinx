014feedc70f6d4dcd498619697a0ae46566c8df5
phoeinx <hradilak@uni-potsdam.de>
2019-11-06 15:06:05 +0100

    Add failing tests for #9

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should not accept papers with empty values for title' do
+    @paper = Paper.new(title: '', venue: 'Great venue', year: 144)
+    expect(@paper).to_not be_valid
+    @paper.update(title: 'Great title')
+    expect(@paper).to be_valid
+  end
+
+  it 'should not accept papers with empty values for venue' do
+    @paper = Paper.new(title: 'Great title', venue: '', year: 144)
+    expect(@paper).to_not be_valid
+    @paper.update(venue: 'Great venue')
+    expect(@paper).to be_valid
+  end
+
+  it 'should not accept papers with empty values for year' do
+    @paper = Paper.new(title: 'Great title', venue: 'Great venue', year: nil)
+    expect(@paper).to_not be_valid
+    @paper.update(year: 1)
+    expect(@paper).to be_valid
+  end
+
+  it 'should not accept papers with non numerical values for year' do
+    @paper = Paper.new(title: 'Great title', venue: 'Great venue', year: '1442')
+    expect(@paper).to_not be_valid
+    @paper.update(year: 1442)
+    expect(@paper).to be_valid
+  end
+
 end
